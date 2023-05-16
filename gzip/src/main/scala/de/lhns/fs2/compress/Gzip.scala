cc1d3b29ac2890daf46f397648ee5fffac17e004
Pierre Kisters <pierrekisters@gmail.com>
2023-05-17 01:27:24 +0200

    LiftIO

--- gzip/src/main/scala/de/lhns/fs2/compress/Gzip.scala
+++ gzip/src/main/scala/de/lhns/fs2/compress/Gzip.scala
@@ -1,33 +1,29 @@
 package de.lhns.fs2.compress
 
-import cats.effect.Async
+import cats.effect.{Async, LiftIO}
 import fs2.Pipe
-import fs2.compression.Compression
+import fs2.io.compression._
 
-class GzipCompressor[F[_] : Async](deflateLevel: Option[Int],
-                                   deflateStrategy: Option[Int],
-                                   chunkSize: Int) extends Compressor[F] {
-  private val compression: Compression[F] = Compression.forSync
-
+class GzipCompressor[F[_] : Async : LiftIO](deflateLevel: Option[Int],
+                                            deflateStrategy: Option[Int],
+                                            chunkSize: Int) extends Compressor[F] {
   override def compress: Pipe[F, Byte, Byte] =
-    compression.gzip(chunkSize, deflateLevel, deflateStrategy)
+    fs2.compression.Compression[F].gzip(chunkSize, deflateLevel, deflateStrategy)
 }
 
 object GzipCompressor {
-  def apply[F[_] : Async](deflateLevel: Option[Int] = None,
-                          deflateStrategy: Option[Int] = None,
-                          chunkSize: Int = Defaults.defaultChunkSize): GzipCompressor[F] =
+  def apply[F[_] : Async : LiftIO](deflateLevel: Option[Int] = None,
+                                   deflateStrategy: Option[Int] = None,
+                                   chunkSize: Int = Defaults.defaultChunkSize): GzipCompressor[F] =
     new GzipCompressor(deflateLevel, deflateStrategy, chunkSize)
 }
 
-class GzipDecompressor[F[_] : Async](chunkSize: Int) extends Decompressor[F] {
-  private val compression: Compression[F] = Compression.forSync
-
+class GzipDecompressor[F[_] : Async : LiftIO](chunkSize: Int) extends Decompressor[F] {
   override def decompress: Pipe[F, Byte, Byte] =
-    compression.gunzip(chunkSize).andThen(_.flatMap(_.content))
+    fs2.compression.Compression[F].gunzip(chunkSize).andThen(_.flatMap(_.content))
 }
 
 object GzipDecompressor {
-  def apply[F[_] : Async](chunkSize: Int = Defaults.defaultChunkSize): GzipDecompressor[F] =
+  def apply[F[_] : Async : LiftIO](chunkSize: Int = Defaults.defaultChunkSize): GzipDecompressor[F] =
     new GzipDecompressor(chunkSize)
 }
