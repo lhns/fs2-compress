507bc581b70b1c1aee1fbe9bfe11869448597bc8
Jakob Merrild <jm@famly.co>
2024-10-10 19:35:41 +0200

    Add BrotliCompressor
    Now that we are using Brotli4J we can add a BrotliCompressor without
    much hassle

--- brotli/src/main/scala/de/lhns/fs2/compress/Brotli.scala
+++ brotli/src/main/scala/de/lhns/fs2/compress/Brotli.scala
@@ -2,9 +2,34 @@ package de.lhns.fs2.compress
 
 import cats.effect.Async
 import com.aayushatharva.brotli4j.Brotli4jLoader
+import com.aayushatharva.brotli4j.encoder.{BrotliOutputStream, Encoder}
 import com.aayushatharva.brotli4j.decoder.BrotliInputStream
 import fs2.{Pipe, Stream}
 import fs2.io._
+
+class BrotliCompressor[F[_]: Async] private (chunkSize: Int, params: Encoder.Parameters) extends Compressor[F] {
+  override def compress: Pipe[F, Byte, Byte] = { stream =>
+    Stream.exec(Async[F].blocking(Brotli4jLoader.ensureAvailability())).covaryOutput[Byte] ++
+      readOutputStream[F](chunkSize) { outputStream =>
+        stream
+          .through(
+            writeOutputStream[F](Async[F].blocking(new BrotliOutputStream(outputStream, params)))
+          )
+          .compile
+          .drain
+      }
+  }
+}
+
+object BrotliCompressor {
+  def apply[F[_]](implicit instance: BrotliCompressor[F]): BrotliCompressor[F] = instance
+
+  def make[F[_]: Async](
+      chunkSize: Int = Defaults.defaultChunkSize,
+      params: Encoder.Parameters = Encoder.Parameters.DEFAULT
+  ): BrotliCompressor[F] =
+    new BrotliCompressor(chunkSize, params)
+}
 
 class BrotliDecompressor[F[_]: Async] private (chunkSize: Int) extends Decompressor[F] {
   override def decompress: Pipe[F, Byte, Byte] = { stream =>
